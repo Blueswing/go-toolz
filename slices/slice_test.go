6b2cb39416bdb9054279d4a7ea58f5e84e63fc75
Blueswing <yifei.shen@yahoo.com>
2022-03-24 17:46:16 +0800

    test

--- slices/slice_test.go
+++ slices/slice_test.go
@@ -1,10 +1,12 @@
 package slices
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/blueswing/go-toolz/defs"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/slices"
 )
 
 func TestContains(t *testing.T) {
@@ -26,4 +28,12 @@ func TestAccumulate(t *testing.T) {
 	s := []int{1, 2, 3, 4, 5}
 	res := Accumulate(0, s, func(x, y int) int { return x + y })
 	assert.Equal(t, 15, res)
+}
+
+func TestMap(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	exp := []string{"1", "2", "3", "4", "5"}
+	res := Map[int, string](s, func(i int) string { return strconv.Itoa(i) })
+	ans := slices.EqualFunc(res, exp, func(x, y string) bool { return x == y })
+	assert.True(t, ans)
 }
